ed876f8e928718b60310ffbc0c3223521be9d5ad
InBrewJ <jason.brewer101@gmail.com>
2017-06-30 19:30:13 +0100

    added prosposed design document

---
+++ source/test/ClangTest/ClangTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClangTest
+//
+//  Created by Jason  Brewer on 28/06/2017.
+//  Copyright © 2017 Jason Brewer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "C++ standard is: " << __cplusplus << "\n";
+    return 0;
+}
