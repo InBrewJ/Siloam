f290e956f9c3c8b41bf7af8164b6cb901e476471
InBrewJ <jason.brewer101@gmail.com>
2017-08-01 22:49:26 +0100

    ImportAndRender.py now covers steps 1-5 of the blender workflow

--- source/2-learn/blenderScripts/ImportAndRender.py
+++ source/2-learn/blenderScripts/ImportAndRender.py
@@ -1,8 +1,8 @@
 #
 # ImportAndRender.py
 # 
-# Currently steps 1 and 2 of the Blender workflow - import a spheremap, rotate it 
-# and add lights
+# Currently steps 1-5 of the Blender workflow - import a spheremap, rotate it 
+# and add lights, then render RGB-D images
 #
 # It's advisable to save a startup file with a blank scene
 # (i.e. with the cube and lamp deleted) before running this script
@@ -27,30 +27,39 @@ def clearScene():
 def toRad(angle):
     return angle*(math.pi/180.0)
 
-def createCameras(scene):
+def createCameras(scene, context):
+    cameraId = 0
     for angle in [0, 45, 90, 135, 180, 225, 270, 315]:
         
         # Create the forwards facing cameras
         
-        cameraData = bpy.data.cameras.new("Camera")
-        camera = bpy.data.objects.new("Camera", cameraData)
+        # name the camera appropriately
+        
+        cameraData = bpy.data.cameras.new("forwards" + str(cameraId))
+        camera = bpy.data.objects.new("forwards" + str(cameraId), cameraData)
         camera.location = (-0.2, 0.0, -0.6)
         # Euler angles are in radians NOT degrees
         camera.rotation_euler = (toRad(90), 0.0, toRad(-90 + angle))
         camera.data.angle = toRad(fov)
+        #camera.data.name = "forwards" + str(cameraId)
     
         scene.objects.link(camera)
         
         # create the downwards facing cameras to view the heading widget
+
+        # name the camera appropriately
         
-        cameraData = bpy.data.cameras.new("Camera")
-        camera = bpy.data.objects.new("Camera", cameraData)
+        cameraData = bpy.data.cameras.new("downwards"  + str(cameraId))
+        camera = bpy.data.objects.new("downwards"  + str(cameraId), cameraData)
         camera.location = (-0.2, 0.0, -0.6)
         # Euler angles are in radians NOT degrees
         camera.rotation_euler = (0.0, 0.0, toRad(-90 + angle))
         camera.data.angle = toRad(fov)
+        camera.data.name = "downwards"  + str(cameraId)
     
         scene.objects.link(camera)
+        
+        cameraId += 1
     
 
 def render(objPath, folder):
@@ -73,7 +82,7 @@ def render(objPath, folder):
     
     # Create the cameras
     
-    createCameras(scene)
+    createCameras(scene, context)
         
     # Create an omidirectional point light without specular light to 
     # illuminate the textures as they were captured   
@@ -113,13 +122,67 @@ def render(objPath, folder):
     for c in cams:
         context.scene.camera = c                                    
         print("Render ", objName, context.scene.name, c.name)
-        bpy.data.scenes[objName].render.filepath = os.path.join(*["/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/renders", folder, c.name])
+        bpy.data.scenes[objName].render.filepath = os.path.join(*["/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/renders/SiloamSee", folder, c.name + "-RGB"])
         bpy.ops.render.render(write_still=True)
         
+        # set up nodes for creating depth maps
+        
+        if "forwards" in c.name:
+            print("Generating depth map for: " + c.name)
+            # switch on nodes and get reference
+            bpy.context.scene.use_nodes = True
+            tree = context.scene.node_tree
+            
+            print("Removing default nodes")
+            
+            # clear default nodes
+            for node in tree.nodes:
+                tree.nodes.remove(node)
+                
+            # Create the render layers, map range and composite nodes
+                
+            RLNode = tree.nodes.new(type='CompositorNodeRLayers')
+            RLNode.location = 0,0
+            
+            MRNode = tree.nodes.new(type='CompositorNodeMapRange')
+            MRNode.location = 210,-100
+            
+            # set the distance thresholds
+            
+            MRNode.inputs[1].default_value = 0.720
+            MRNode.inputs[2].default_value = 2.750
+            MRNode.inputs[3].default_value = 1.000
+            MRNode.inputs[4].default_value = 0.000
+            
+            CNode = tree.nodes.new(type='CompositorNodeComposite')
+            CNode.location = 400,0
+            
+            # link the nodes
+            
+            links = tree.links
+            link = links.new(RLNode.outputs[2], MRNode.inputs[0])
+            link = links.new(MRNode.outputs[0], CNode.inputs[0])
+            
+            # render the depth image
+            
+            bpy.data.scenes[objName].render.filepath = os.path.join(*["/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/renders/SiloamSee", folder, c.name + "-D"])
+            bpy.ops.render.render(write_still=True)
+                
+            # clear nodes again and disable nodes
+            for node in tree.nodes:
+                tree.nodes.remove(node)
+             
+            bpy.context.scene.use_nodes = False    
+            
+             
     # copy the .dat file from the obj folder into the render folder
     print("Copying dat file...")
-    copyfile(os.path.join(modelPath, "gpsAndHeading.dat"), os.path.join(*["/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/renders", folder, "gpsAndHeading.dat"]))
-            
+    copyfile(os.path.join(modelPath, "gpsAndHeading.dat"), os.path.join(*["/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/renders/SiloamSee", folder, "gpsAndHeading.dat"]))
+        
+def renderOnce():
+    clearScene()
+    render("/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/models/obj/", "SiloamSee-170725-203831-train")
+
 # run() function - to be called from the python console window
 
 # My path for the OBJs from SiloamSee
@@ -144,6 +207,3 @@ def run(objPath):
         render(objPath, folder)
     
     print("Done")
-    
-def renderOnce():
-    render("/Users/LordNelson/Documents/Work/LiverpoolUni/DissertationStore/2-learn/models/obj/", "SiloamSee-170725-203831-train")
