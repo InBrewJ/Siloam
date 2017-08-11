3f9d4c93938e435d2f15153afb9b4901bc75d402
InBrewJ <jason.brewer101@gmail.com>
2017-08-11 07:36:17 +0100

    Added the XCode project of SiloamLearn

---
+++ source/2-learn/SiloamLearn/SiloamLearn/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SiloamLearn
+//
+//  Created by Jason  Brewer on 11/08/2017.
+//  Copyright © 2017 University of Liverpool. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "SVM testing complete! Nah, just kidding\n";
+    
+    return 0;
+}
