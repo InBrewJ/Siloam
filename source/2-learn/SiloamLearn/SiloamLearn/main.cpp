acea1844385d3525d3abf11764bf8966d79fd6e7
InBrewJ <jason.brewer101@gmail.com>
2017-08-12 16:43:43 +0100

    Added meeting summary for 11/08/2017

--- source/2-learn/SiloamLearn/SiloamLearn/main.cpp
+++ source/2-learn/SiloamLearn/SiloamLearn/main.cpp
@@ -1,6 +1,10 @@
+//  NB: BUILT WITH XCODE 8.3.3 (8E3004b) on macOS Sierra 10.12.5
+//
 //
 //  main.cpp
 //  SiloamLearn
+//
+//
 //
 //  Created by Jason  Brewer on 11/08/2017.
 //  Copyright © 2017 University of Liverpool. All rights reserved.
